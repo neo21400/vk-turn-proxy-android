03703d590f1d2b64cc5ae71bc5f106fbab9c9e8a
neo21400 <61691793+neo21400@users.noreply.github.com>
2026-03-24 11:23:07 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
     buildTypes {
         debug {
-            isMinifyEnabled = true 
+            isMinifyEnabled = false
             isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
